2096fbfe1e51ba8978f057ba0c15979fae9d511e
Noel Peterson <noel.marcus.peterson@gmail.com>
2013-06-03 09:25:32 -0500

    Updated to support SAS 9.3 installations

--- sasrun_gdb.bat
+++ sasrun_gdb.bat
@@ -1,6 +1,6 @@
 @echo off
 REM sasrun.bat
-REM   Craig Heither, last revised 04/05/2011
+REM   cheither & npeterson, last revised 06/01/2013
 REM
 REM    This is called by various Python scripts to execute a specified SAS program.
 REM    SAS paths are defined (now that all pcs should be on Windows 7) in order to send the appropriate SAS command. 
@@ -12,10 +12,12 @@ REM        %3: SAS log file name
 REM        %4: SAS list file name
 REM #################################################
 
-set saspath="C:\Program Files\SAS\SASFoundation\9.2(32-bit)\sas.exe"
-set saspath1="C:\Program Files\SAS\SASFoundation\9.2\sas.exe"
+set saspath="C:\Program Files\SASHome\SASFoundation\9.3\sas.exe"
+set saspath1="C:\Program Files\SAS\SASFoundation\9.2(32-bit)\sas.exe"
+set saspath2="C:\Program Files\SAS\SASFoundation\9.2\sas.exe"
 
 if not exist %saspath% (set saspath=%saspath1%)
+if not exist %saspath% (set saspath=%saspath2%)
 if not exist %saspath% (goto badsas)
 
 REM - Run SAS
