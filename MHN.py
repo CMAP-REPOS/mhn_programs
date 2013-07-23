fd2d312533645301f75beeaec003993ee0b91280
Noel Peterson <noel.marcus.peterson@gmail.com>
2013-07-23 15:57:56 -0500

    Move shortest path function to MHN module

--- MHN.py
+++ MHN.py
@@ -225,6 +225,37 @@ def ensure_dir(directory):
     return directory
 
 
+def find_shortest_path(graph, start, end, visited=None, distances=None, predecessors=None):
+    ''' Recursive function written by nolfonzo@gmail.com to find shortest path
+        between 2 nodes in a graph; implementation of Dijkstra's algorithm.
+        Based on <http://rebrained.com/?p=392>, accessed 9/2011. '''
+    if visited == None:
+        visited = []
+    if distances == None:
+        distances = {}
+    if predecessors == None:
+        predecessors = {}
+    if not visited:
+        distances[start] = 0  # Set distance to 0 for first pass
+    if start == end:  # We've found our end node, now find the path to it, and return
+        path = []
+        while end != None:
+            path.append(end)
+            end = predecessors.get(end, None)
+        return distances[start], path[::-1]
+    for neighbor in graph[start]:  # Process neighbors, keep track of predecessors
+        if neighbor not in visited:
+            neighbordist = distances.get(neighbor, sys.maxsize)
+            tentativedist = distances[start] + graph[start][neighbor]
+            if tentativedist < neighbordist:
+                distances[neighbor] = tentativedist
+                predecessors[neighbor] = start
+    visited.append(start)  # Mark the current node as visited
+    unvisiteds = dict((k, distances.get(k, sys.maxsize)) for k in graph if k not in visited)  # Finds the closest unvisited node to the start
+    closestnode = min(unvisiteds, key=unvisiteds.get)
+    return shortestpath(graph, closestnode, end, visited, distances, predecessors)  # Start processing the closest node
+
+
 def get_yearless_hwyproj():
     ''' Check hwyproj completion years and return list of invalid projects'
         TIPIDs. '''
--- shortest_path.py
+++ shortest_path.py
@@ -2,20 +2,17 @@
 '''
     shortest_path.py
     Authors: cheither & npeterson
-    Revised: 5/6/13
+    Revised: 7/23/13
     ---------------------------------------------------------------------------
-    This script finds the shortest path between two nodes, given the available
-    network. The source of the shortest path function is:
-
-      <http://rebrained.com/?p=392> (accessed 09/2011) - author unknown
-
-    The function uses a brute force method to determine the shortest path: a
-    bit inelegant but effective.
+    This script finds the shortest path between two nodes, using a network
+    graph read in from a CSV. It is essentially a wrapper of the MHN module's
+    find_shortest_path() function, to facilitate calls from SAS programs.
 
 '''
 from __future__ import print_function
 import csv
 import sys
+import MHN
 
 # -----------------------------------------------------------------------------
 #  Set parameters.
@@ -30,38 +27,15 @@ sys.setrecursionlimit(6000)  # Max. iterations (sys default = 1000)
 # -----------------------------------------------------------------------------
 #  Find shortest path.
 # -----------------------------------------------------------------------------
+print('Finding shortest path from {0} to {1}...'.format(anode, bnode))
+
 graph = {}
 reader = csv.reader(open(link_dict_txt), delimiter='$')
 for row in reader:
     graph[eval(row[0])] = eval(row[1]) # Assign key/value pairs
 
-def shortestpath(graph, start, end, visited=[], distances={}, predecessors={}):
-    ''' Function written by unknown author to find shortest path between 2
-        nodes in a graph; implementation of Dijkstra's algorithm. '''
-    if not visited:
-        distances[start] = 0  # Set distance to 0 for first pass
-    if start == end:  # We've found our end node, now find the path to it, and return
-        path = []
-        while end != None:
-            path.append(end)
-            end = predecessors.get(end, None)
-        return distances[start], path[::-1]
-    for neighbor in graph[start]:  # Process neighbors, keep track of predecessors
-        if neighbor not in visited:
-            neighbordist = distances.get(neighbor, sys.maxsize)
-            tentativedist = distances[start] + graph[start][neighbor]
-            if tentativedist < neighbordist:
-                distances[neighbor] = tentativedist
-                predecessors[neighbor] = start
-    visited.append(start)  # Mark the current node as visited
-    unvisiteds = dict((k, distances.get(k, sys.maxsize)) for k in graph if k not in visited)  # Finds the closest unvisited node to the start
-    closestnode = min(unvisiteds, key=unvisiteds.get)
-    return shortestpath(graph, closestnode, end, visited, distances, predecessors)  # Start processing the closest node
-
-print(str(anode) + ',' + str(bnode))
-
-outFile = open(short_path_txt, 'a')
-outFile.write(str(shortestpath(graph, eval(anode), eval(bnode))) + '\n')
-outFile.close()
+short_path = open(short_path_txt, 'a')
+short_path.write(str(MHN.find_shortest_path(graph, eval(anode), eval(bnode))) + '\n')
+short_path.close()
 
 print('DONE')
