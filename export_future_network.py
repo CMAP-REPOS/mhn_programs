b94e35ffc7227eb4dacfeeb7cc5cdc77888f1e39
Noel Peterson <nmpeterson@users.noreply.github.com>
2014-02-06 12:17:45 -0600

    Improved path handling

--- export_future_network.py
+++ export_future_network.py
@@ -2,7 +2,7 @@
 '''
     export_future_network.py
     Author: npeterson
-    Revised: 7/25/13
+    Revised: 2/6/14
     ---------------------------------------------------------------------------
     Build the MHN to its coded state for a specified year, and save the future
     arcs and nodes in a specified GDB. This is particularly for building the
@@ -28,12 +28,12 @@ gdb_path = arcpy.GetParameterAsText(1)  # Folder, no default
 gdb_name = arcpy.GetParameterAsText(2)  # String, no default
 if not gdb_name.endswith('.gdb'):
     gdb_name = '{0}.gdb'.format(gdb_name)
-out_gdb = '/'.join((gdb_path, gdb_name))
+out_gdb = os.path.join(gdb_path, gdb_name)
 if not arcpy.Exists(out_gdb):
     arcpy.CreateFileGDB_management(gdb_path, gdb_name)
 
 out_fd_name = 'hwynet_{0}'.format(build_year)
-out_fd = '/'.join((out_gdb, out_fd_name))
+out_fd = os.path.join(out_gdb, out_fd_name)
 sr = arcpy.Describe(MHN.hwynet).spatialReference
 if not arcpy.Exists(out_fd):
     arcpy.CreateFeatureDataset_management(out_gdb, out_fd_name, sr)
@@ -45,13 +45,13 @@ sas1_name = 'export_future_network_2'
 # -----------------------------------------------------------------------------
 #  Set diagnostic output locations.
 # -----------------------------------------------------------------------------
-sas1_log = ''.join((MHN.temp_dir, '/', sas1_name, '.log'))
-sas1_lst = ''.join((MHN.temp_dir, '/', sas1_name, '.lst'))
-year_csv = '/'.join((MHN.temp_dir, 'year.csv'))
-transact_csv = '/'.join((MHN.temp_dir, 'transact.csv'))
-network_csv = '/'.join((MHN.temp_dir, 'network.csv'))
-update_link_csv = '/'.join((MHN.temp_dir, 'update_link.csv'))  # SAS output
-flag_node_csv = '/'.join((MHN.temp_dir, 'flag_node.csv'))      # SAS output
+sas1_log = os.path.join(MHN.temp_dir, '{0}.log'.format(sas1_name))
+sas1_lst = os.path.join(MHN.temp_dir, '{0}.lst'.format(sas1_name))
+year_csv = os.path.join(MHN.temp_dir, 'year.csv')
+transact_csv = os.path.join(MHN.temp_dir, 'transact.csv')
+network_csv = os.path.join(MHN.temp_dir, 'network.csv')
+update_link_csv = os.path.join(MHN.temp_dir, 'update_link.csv')  # SAS output
+flag_node_csv = os.path.join(MHN.temp_dir, 'flag_node.csv')      # SAS output
 
 
 # -----------------------------------------------------------------------------
@@ -99,11 +99,11 @@ network_lyr = MHN.make_skinny_feature_layer(MHN.arc, 'network_lyr', network_attr
 MHN.write_attribute_csv(network_lyr, network_csv, network_attr)
 
 # Process attribute tables with export_future_network_2.sas.
-sas1_sas = ''.join((MHN.prog_dir, '/', sas1_name, '.sas'))
+sas1_sas = os.path.join(MHN.prog_dir, '{0}.sas'.format(sas1_name))
 sas1_args = [network_csv, transact_csv, year_csv, update_link_csv, flag_node_csv, build_year, MHN.max_poe, MHN.base_year]
 MHN.submit_sas(sas1_sas, sas1_log, sas1_lst, sas1_args)
 if not os.path.exists(sas1_log):
-    MHN.die(sas1_sas + ' did not run!')
+    MHN.die('{0} did not run!'.format(sas1_sas))
 else:
     os.remove(sas1_log)
     MHN.delete_if_exists(sas1_lst)
@@ -130,7 +130,7 @@ with open(update_link_csv, 'r') as reader:
         update_link_dict[abb] = attr
 
 out_arc_name = 'hwynet_arc_{0}'.format(build_year)
-temp_arc = '/'.join((MHN.mem, out_arc_name))
+temp_arc = os.path.join(MHN.mem, out_arc_name)
 arcpy.FeatureClassToFeatureClass_conversion(MHN.arc, MHN.mem, out_arc_name)
 arcpy.AddField_management(temp_arc, 'ACTION_CODE', 'TEXT', field_length=1)
 arcpy.CalculateField_management(temp_arc, 'ACTION_CODE', "'0'", 'PYTHON')
@@ -145,7 +145,7 @@ with arcpy.da.UpdateCursor(temp_arc, update_link_attr) as cursor:
             cursor.updateRow(update_link_dict[abb])
 
 # Copy future baselinks to output GDB.
-out_arc = '/'.join((out_fd, out_arc_name))
+out_arc = os.path.join(out_fd, out_arc_name)
 temp_arc_lyr = 'temp_arc_lyr'
 temp_arc_query = ''' "BASELINK" = '1' '''
 arcpy.MakeFeatureLayer_management(temp_arc, temp_arc_lyr, temp_arc_query)
@@ -166,7 +166,7 @@ with open(flag_node_csv, 'r') as reader:
         flag_node_dict[node] = attr
 
 out_node_name = 'hwynet_node_{0}'.format(build_year)
-temp_node = '/'.join((MHN.mem, out_node_name))
+temp_node = os.path.join(MHN.mem, out_node_name)
 arcpy.FeatureClassToFeatureClass_conversion(MHN.node, MHN.mem, out_node_name)
 for fieldname in ('ARTERIAL', 'RAMP', 'GHOST'):
     arcpy.AddField_management(temp_node, fieldname, 'SHORT')
@@ -180,7 +180,7 @@ with arcpy.da.UpdateCursor(temp_node, flag_node_attr) as cursor:
             cursor.updateRow(flag_node_dict[node])
 
 # Copy future nodes to output GDB.
-out_node = '/'.join((out_fd, out_node_name))
+out_node = os.path.join(out_fd, out_node_name)
 #out_node_query = ''' "ARTERIAL" + "RAMP" + "GHOST" > 0 '''
 temp_node_lyr = 'temp_node_lyr'
 arcpy.MakeFeatureLayer_management(temp_node, temp_node_lyr)
