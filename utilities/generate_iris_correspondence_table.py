96f79d3f7125a47d71e35462e6cd9ce0b9e84dc4
Noel Peterson <nmpeterson@users.noreply.github.com>
2014-04-14 11:35:39 -0500

    Improved fuzzy string matching

--- utilities/generate_iris_correspondence_table.py
+++ utilities/generate_iris_correspondence_table.py
@@ -2,7 +2,7 @@
 '''
     generate_iris_correspondence_table.py
     Author: npeterson
-    Revised: 4/11/2014
+    Revised: 4/14/2014
     ---------------------------------------------------------------------------
     Generate an "mhn2iris" correspondence table from the current MHN. Useful
     after extensive geometric updates or network expansion.
@@ -61,7 +61,7 @@ iris_clipped = os.path.join(MHN.mem, 'iris_clipped')
 arcpy.Clip_analysis(iris_fc, illinois_lyr, iris_clipped)
 iris_arts_fc = os.path.join(temp_gdb, 'iris_arts')
 iris_arts_vertices_fc = os.path.join(temp_gdb, 'iris_arts_vertices')
-iris_arts_fields = [iris_id_field, 'ROAD_NAME', 'MARKED_RT']
+iris_arts_fields = [iris_id_field, 'ROAD_NAME', 'MARKED_RT', 'MARKED_RT2']
 iris_arts_query = ''' "FCNAME" NOT IN ('Freeway and Expressway','Interstate') '''
 iris_arts_lyr = MHN.make_skinny_feature_layer(iris_clipped, 'iris_arts_lyr', iris_arts_fields, iris_arts_query)
 arcpy.CopyFeatures_management(iris_arts_lyr, iris_arts_fc)
@@ -112,7 +112,7 @@ mhn_attr_dict = MHN.make_attribute_dict(mhn_arts_lyr, 'ABB', ['ROADNAME'])
 
 matched_iris_ids = set([str(row[0]) for row in arcpy.da.SearchCursor(mhn_near_iris_freq_table, [near_iris_field])])
 arcpy.SelectLayerByAttribute_management(iris_arts_lyr, 'NEW_SELECTION', ''' "{0}" IN ({1}) '''.format(iris_id_field, ','.join(matched_iris_ids)))
-iris_attr_dict = MHN.make_attribute_dict(iris_arts_lyr, iris_id_field, ['ROAD_NAME', 'MARKED_RT'])
+iris_attr_dict = MHN.make_attribute_dict(iris_arts_lyr, iris_id_field, ['ROAD_NAME', 'MARKED_RT', 'MARKED_RT2'])
 
 # Define some cleaning functions for MHN/IRIS road names & rte numbers.
 def clean_name(in_name):
@@ -121,20 +121,18 @@ def clean_name(in_name):
     # Ignore ramps (mostly in IRIS):
     if ' TO ' not in out_name:
         # Replace punctuation and misc. keywords:
-        for string, rep in [('-', ' '), ('/', ' '), ('(', ''), (')', ''), ('.', ''), ("'", ""), ('MARTIN LUTHER KING', 'MLK')]:
+        for string, rep in [('-',' '),('/',' '),('(',''),(')',''),('.',''),("'",""),('MARTIN LUTHER KING', 'MLK')]:
             out_name = out_name.replace(string, rep)
         # Remove cardinal directions:
-        for cdir in ('N', 'S', 'E', 'W'):
+        for cdir in ('N','S','E','W'):
             if out_name.startswith(cdir + ' '):
                 out_name = out_name[len(cdir):].strip()
             if out_name.endswith(' ' + cdir):
                 out_name = out_name[:-len(cdir)].strip()
-        # Remove suffixes (road types):
-        for suf in ('AVE', 'AV', 'BLVD', 'CT', 'DR', 'EXPY', 'HWY', 'LN', 'PKWY', 'PKY', 'PL', 'RD', 'ST', 'TR', 'WAY'):
-            if out_name.endswith(' ' + suf):
-                out_name = out_name[:-len(suf)].strip()
-        ## Find longest remaining "word":
-        #out_name = max(out_name.split(' '), key=len)
+        # Remove suffixes (road types and directions):
+        for suf in ('NB','SB','EB','WB','AVE','AV','BLVD','CT','DR','EXPY','HWY','LN','PKWY','PKY','PL','RD','SQ','ST','TR','WAY'):
+            if out_name.endswith(' ' + suf) or ' ' + suf + ' ' in out_name:
+                out_name = out_name.replace(' ' + suf, '')
     else:
         out_name = ''
     return out_name
@@ -159,123 +157,37 @@ with arcpy.da.SearchCursor(mhn_near_iris_freq_table, [near_mhn_field, near_iris_
         mhn_name = clean_name(mhn_attr_dict[mhn_id]['ROADNAME'])
         iris_name = clean_name(iris_attr_dict[iris_id]['ROAD_NAME'])
         iris_rte = clean_rte(iris_attr_dict[iris_id]['MARKED_RT'])
-        iris_combo = '{0} {1}'.format(iris_rte, iris_name).strip()
+        iris_rte2 = clean_rte(iris_attr_dict[iris_id]['MARKED_RT2'])
+        iris_combo = '{0} {1} {2}'.format(iris_rte, iris_rte2, iris_name).strip().replace('  ', ' ')
 
         fuzz_score = fuzz.token_set_ratio(mhn_name, iris_combo)  # 0-100: How similar are the names?
 
-        # Initially, match any pair meeting min_match_count criterion
+        # Make initial match if min match count and fuzz_score are okay
         if mhn_id not in match_dict:
             if freq > min_match_count:
-                match_dict[mhn_id] = (iris_id, freq, fuzz_score)
 
-        # Consider replacing match if match count is at least as high
+                # Give the benefit of the doubt when either is unnamed
+                if not (mhn_name or iris_combo):
+                    match_dict[mhn_id] = (iris_id, freq, fuzz_score, mhn_name, iris_combo)
+
+                # Otherwise only match if fuzz_score is better (and above minimum threshold)
+                elif fuzz_score > min_fuzz_score:
+                    match_dict[mhn_id] = (iris_id, freq, fuzz_score, mhn_name, iris_combo)
+
+        # Consider replacing match if new match count is at least as high
         elif freq > match_dict[mhn_id][1]:
+
             # Give the benefit of the doubt when either is unnamed
             if not (mhn_name or iris_combo):
-                match_dict[mhn_id] = (iris_id, freq, fuzz_score)
+                match_dict[mhn_id] = (iris_id, freq, fuzz_score, mhn_name, iris_combo)
+
             # Otherwise only match if fuzz_score is better (and above minimum threshold)
             elif fuzz_score > max(min_fuzz_score, match_dict[mhn_id][2]):
-                match_dict[mhn_id] = (iris_id, freq, fuzz_score)
+                match_dict[mhn_id] = (iris_id, freq, fuzz_score, mhn_name, iris_combo)
 
         elif freq == match_dict[mhn_id][1]:
             if fuzz_score > max(min_fuzz_score, match_dict[mhn_id][2]):
-                match_dict[mhn_id] = (iris_id, freq, fuzz_score)
-
-
-## -----------------------------------------------------------------------------
-##  Perform QC tests to filter out unlikely matches.
-## -----------------------------------------------------------------------------
-#arcpy.AddMessage('\nRunning QC tests on potential matches...')
-#
-## Create dictionaries of attributes (road name, rte number) for any matched MHN
-## and IRIS links.
-#matched_mhn_ids = (str(mhn_id) for mhn_id in match_dict)
-#arcpy.SelectLayerByAttribute_management(mhn_arts_lyr, 'NEW_SELECTION', ''' "ABB" IN ('{0}') '''.format("','".join(matched_mhn_ids)))
-#mhn_attr_dict = MHN.make_attribute_dict(mhn_arts_lyr, 'ABB', ['ROADNAME'])
-#
-#matched_iris_ids = (str(match_dict[mhn_id][0]) for mhn_id in match_dict)
-#arcpy.SelectLayerByAttribute_management(iris_arts_lyr, 'NEW_SELECTION', ''' "{0}" IN ({1}) '''.format(iris_id_field, ','.join(matched_iris_ids)))
-#iris_attr_dict = MHN.make_attribute_dict(iris_arts_lyr, iris_id_field, ['ROAD_NAME', 'MARKED_RT'])
-#
-## Define some cleaning functions for MHN/IRIS road names & rte numbers.
-#def clean_name(in_name):
-#    ''' Remove punctuation, cardinal directions, and suffixes '''
-#    out_name = in_name.upper()
-#    # Ignore ramps (mostly in IRIS):
-#    if ' TO ' not in out_name:
-#        # Replace punctuation and misc. keywords:
-#        for string, rep in [('-', ' '), ('/', ' '), ('(', ''), (')', ''), ('.', ''), ("'", ""), ('MARTIN LUTHER KING', 'MLK')]:
-#            out_name = out_name.replace(string, rep)
-#        # Remove cardinal directions:
-#        for cdir in ('N', 'S', 'E', 'W'):
-#            if out_name.startswith(cdir + ' '):
-#                out_name = out_name[len(cdir):].strip()
-#            if out_name.endswith(' ' + cdir):
-#                out_name = out_name[:-len(cdir)].strip()
-#        # Remove suffixes (road types):
-#        for suf in ('AVE', 'AV', 'BLVD', 'CT', 'DR', 'EXPY', 'HWY', 'LN', 'PKWY', 'PKY', 'PL', 'RD', 'ST', 'TR', 'WAY'):
-#            if out_name.endswith(' ' + suf):
-#                out_name = out_name[:-len(suf)].strip()
-#        # Find longest remaining "word":
-#        out_name = max(out_name.split(' '), key=len)
-#    else:
-#        out_name = None
-#    return out_name
-#
-#def clean_rte(in_rte):
-#    ''' Convert IRIS format into probable MHN format '''
-#    if in_rte.startswith('S'):
-#        out_rte = 'IL {0}'.format(in_rte[1:].lstrip('0'))
-#    elif in_rte.startswith('U'):
-#        out_rte = 'US {0}'.format(in_rte[1:].lstrip('0'))
-#    else:
-#        out_rte = None
-#    return out_rte
-#
-#
-## Iterate through all potential matches and apply QC tests before writing
-## successful matches to a list.
-#qc_matches = []
-#for mhn_id in match_dict:
-#    iris_id = match_dict[mhn_id][0]
-#    mhn_name = mhn_attr_dict[mhn_id]['ROADNAME']
-#    mhn_name_base = clean_name(mhn_name)
-#    iris_name = iris_attr_dict[iris_id]['ROAD_NAME']
-#    iris_name_base = clean_name(iris_name)
-#    iris_rte = clean_rte(iris_attr_dict[iris_id]['MARKED_RT'])
-#
-#    match = False  # Assume no match to begin
-#
-#    # Compare names/route numbers for match:
-#    if mhn_name_base:
-#        if iris_rte:
-#            if iris_rte in mhn_name:
-#                match = True
-#        elif iris_name_base:
-#            if mhn_name_base == iris_name_base:
-#                match = True
-#            elif iris_name_base in mhn_name:
-#                match = True
-#            elif mhn_name_base in iris_name:
-#                match = True
-#    elif not mhn_name_base or not (iris_name_base or iris_rte):
-#        # Give the benefit of the doubt when either link is unnamed
-#        match = True
-#
-#    # Exclude any mistakenly matched 'upper'/'lower' roads:
-#    if match:
-#        if 'LOWER' in mhn_name and 'LOWER' not in iris_name:
-#            match = False
-#        elif 'LOWER' not in mhn_name and 'LOWER' in iris_name:
-#            match = False
-#        elif 'UPPER' in mhn_name and 'UPPER' not in iris_name:
-#            match = False
-#        elif 'UPPER' not in mhn_name and 'UPPER' in iris_name:
-#            match = False
-#
-#    # If matched, add to list:
-#    if match:
-#        qc_matches.append((mhn_id, iris_id))
+                match_dict[mhn_id] = (iris_id, freq, fuzz_score, mhn_name, iris_combo)
 
 
 # -----------------------------------------------------------------------------
@@ -291,13 +203,11 @@ arcpy.AddField_management(match_table, match_mhn_field, 'TEXT', field_length=13)
 arcpy.AddField_management(match_table, match_iris_field, 'LONG')
 arcpy.AddField_management(match_table, 'FREQUENCY', 'LONG')
 arcpy.AddField_management(match_table, 'FUZZ_SCORE', 'LONG')
-with arcpy.da.InsertCursor(match_table, [match_mhn_field, match_iris_field]) as cursor:
-    #for mhn_id, iris_id in qc_matches:
+arcpy.AddField_management(match_table, 'MHN_NAME', 'TEXT', field_length=50)
+arcpy.AddField_management(match_table, 'IRIS_NAME', 'TEXT', field_length=62)
+with arcpy.da.InsertCursor(match_table, [match_mhn_field, match_iris_field, 'FREQUENCY', 'FUZZ_SCORE', 'MHN_NAME', 'IRIS_NAME']) as cursor:
     for mhn_id in match_dict.keys():
-        iris_id = match_dict[mhn_id][0]
-        freq = match_dict[mhn_id][1]
-        fuzz_score = match_dict[mhn_id][2]
-        cursor.insertRow([mhn_id, iris_id])
+        cursor.insertRow([mhn_id] + list(match_dict[mhn_id]))
 
 table_name = table_name.format(MHN.timestamp('%Y%m%d'))
 output_table = arcpy.TableToTable_conversion(match_table, out_workspace, table_name)
@@ -308,5 +218,4 @@ output_table = arcpy.TableToTable_conversion(match_table, out_workspace, table_n
 # -----------------------------------------------------------------------------
 arcpy.AddMessage('\nCleaning up...')
 arcpy.Delete_management(MHN.mem)
-arcpy.Delete_management(temp_gdb)
 arcpy.AddMessage('\nAll done! Correspondence table successfully written to {0}\n'.format(output_table))
